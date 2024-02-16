5d8079c830b45bdac088dbabe853f6daf3fa7a56
wolfscatt <bjkeses09@gmail.com>
2024-02-16 10:07:49 +0300

    info screen updated

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -3,6 +3,10 @@ const String logoImage = 'assets/images/Ecocycle.png';
 const String googleLogo = 'assets/images/google.png';
 const String defaultUserImage = 'assets/images/default-user.jpg';
 const String omerPhoto = 'assets/images/ProfilPhoto2.jpg';
+const String pembenazPhoto = 'assets/images/pembenaz.jpg';
+const String batuPhoto = 'assets/images/batu.jpg';
+const String mervePhoto = 'assets/images/merve.jpg';
+
 
 // ** --------- Text ---------**
 const String welcomeText = 'Welcome to Eco Cycle';
